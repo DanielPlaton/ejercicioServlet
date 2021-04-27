e1d8f85ccbee271d0d6d605ccf47eb3ee16bbd62
DanielPlaton <dcp0495@gmail.com>
2021-04-27 20:38:00 +0200

    tabla html

--- src/main/java/servlet/webEjercicioServlet.java
+++ src/main/java/servlet/webEjercicioServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import DAO.DepartamentoDAO;
+import controlador.MyLogger;
+import modelo.Departamento;
+import utils.HibernateUtil;
+
 /**
  * Servlet implementation class webEjercicioServlet
  */
 @WebServlet("/webEjercicioServlet")
 public class webEjercicioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	Transaction tx = null;
+	Session session;
+	public static Logger logger = MyLogger.crearLogger(webEjercicioServlet.class);
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +42,10 @@ public class webEjercicioServlet extends HttpServlet {
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+		logger.info("Iniciando programa");
+		session = HibernateUtil.getSessionFactory().openSession();
+		
+
 	}
 
 	/**
@@ -35,7 +53,26 @@ public class webEjercicioServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	
+		
+		List<Departamento> listaDepartamentos  = DepartamentoDAO.getAllDepartamentos(session);
+
+		for(int i=0;i<listaDepartamentos.size();i++) {
+			
+			//response.getWriter().append("Lista de usuarios ").append(listaDepartamentos.get(i).toString());
+			response.getWriter().append("<html>");
+			response.getWriter().append("<head>");
+			response.getWriter().append("<title>ejercicio</title>");
+			response.getWriter().append("</head>");
+			response.getWriter().append("<body>");
+			response.getWriter().append("</body>");
+			response.getWriter().append("</html>");
+		
+		
+			
+		}
 	}
 
 	/**
@@ -44,6 +81,7 @@ public class webEjercicioServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	
 	}
 
 }
